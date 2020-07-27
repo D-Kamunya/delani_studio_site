eb3739e1fd564bf75ac1fb7aaad4d2bc810290fd
Dennis Kamunya <dennismuriithik@gmail.com>
2020-07-27 07:53:53 +0300

    Added jquery toggle functionality in what we do section

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(()=>{
+  $('#design').click(()=>{
+    $('#design-img').toggle()
+    $('#design-text').toggle()
+  })
+  $('#dev').click(()=>{
+    $('#dev-img').toggle()
+    $('#dev-text').toggle()
+  })
+  $('#product').click(()=>{
+    $('#product-img').toggle()
+    $('#product-text').toggle()
+  })
+
+})
