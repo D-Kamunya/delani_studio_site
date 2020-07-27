2ab548abeb75434bed6c7a705acb06dff43bef9a
Dennis Kamunya <dennismuriithik@gmail.com>
2020-07-27 13:01:37 +0300

    Added js fn to alert user message received

--- js/index.js
+++ js/index.js
@@ -37,4 +37,14 @@ $(document).ready(()=>{
     $('.projectName8').toggle()
   })
 
+  $('#contactform').submit((event)=>{
+    event.preventDefault()
+    let name=document.getElementById('name').value
+    let email=document.getElementById('name').value
+    let message=document.getElementById('message').value
+
+    alert(`${name}, we have received your message. Thank you for reaching out to us.`)
+    document.getElementById("contactform").reset();
+  })
+
 })
